427e3a479df1747f493d9d8926cb5b1aa6c8f241
Issoufouiha <ayouba.yahoo2000@gmail.com>
2024-10-02 21:28:58 +0100

    tableau numerique

---
+++ tableau.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Premiere en php</title>
+    </head>
+    <body>
+        <?php
+        $myPays = array('Niger', 'benin','Mali','Burkina');
+        echo $myPays[0].'<br>';
+        echo $myPays[1].'<br>';
+        echo $myPays[2].'<br>';
+        echo $myPays[3].'<br>';
+        ?>
+    </body>
+</html>
