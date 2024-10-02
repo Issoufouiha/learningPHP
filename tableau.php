6cb65f60da0da376450328c62724086c09c138b3
Issoufouiha <ayouba.yahoo2000@gmail.com>
2024-10-02 21:50:21 +0100

    tableau associative

--- tableau.php
+++ tableau.php
@@ -10,6 +10,24 @@
         echo $myPays[1].'<br>';
         echo $myPays[2].'<br>';
         echo $myPays[3].'<br>';
+        // methode 2
+        $myPays = ['Niger', 'benin','Mali','Burkina'];
+        echo $myPays[0].'<br>';
+        echo $myPays[1].'<br>';
+        echo $myPays[2].'<br>';
+        echo $myPays[3].'<br>';
+        // tableau associative
+        $tableauAssociative = [
+            'nom'=>'Ayouba',
+            'Prenom'=>'Issoufou',
+            'Pays'=>'Niger',
+            'Captal'=>'Niamey',
+        ];
+     echo   $tableauAssociative['nom'].'<br>';
+     echo   $tableauAssociative['Prenom'].'<br>';
+     echo  $tableauAssociative['Pays'].'<br>';
+     echo  $tableauAssociative['Captal'].'<br>';
         ?>
+
     </body>
 </html>
